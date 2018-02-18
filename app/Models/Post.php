6ab198a5e2f31faae367adc6f47880732b95e42d
andristrieb <andris@andristrieb.com>
2018-02-18 12:40:23 +1000

    Added default Route Key for Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,12 @@ namespace App\Models;
 class Post extends \Enpress\Models\Post
 {
 
-    //
+    /*
+     * Column to use for route model binding
+     */
+    public function getRouteKeyName()
+    {
+        return 'post_name';
+    }
 
 }
